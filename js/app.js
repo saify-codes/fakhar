771c78ca8090ccacffbb6486a1e72dfa86335837
saify-codes <linked119@example.com>
2023-04-12 03:07:35 +0500

    final

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: true,
+        loop: true,
+      },
+    },
+  });
